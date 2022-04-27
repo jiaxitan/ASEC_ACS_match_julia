e3ef0b9cbc395386b2d512f690d4cd65df871657
jiaxitan <101900571+jiaxitan@users.noreply.github.com>
2022-04-27 10:50:28 -0700

    Update match_quality_analysis.jl

--- match_quality_analysis.jl
+++ match_quality_analysis.jl
@@ -86,7 +86,7 @@ matching_set = [:grossinc, :size, :age, :unitsstr_recode, :race_recode, :educ_re
 
 df_ASEC_hh_match_0506_final1 = ASEC_ACS_match([2010, 2011], df_ASEC_hh_match_county, df_ACS_hh_match_county, df_ASEC_hh_match_state, df_ACS_hh_match_state, matching_set);
 
-# Engel curves, percentiles, mean
+# Engel curves, percentiles, mean of nearest neighbors
 insertcols!(df_ASEC_hh_match_0506_final1, size(df_ASEC_hh_match_0506_final1, 2)+1, :valueh => df_ASEC_hh_match_0506_final.ACS_valueh_mean);
 insertcols!(df_ASEC_hh_match_0506_final1, size(df_ASEC_hh_match_0506_final1, 2)+1, :rentgrs => df_ASEC_hh_match_0506_final.ACS_rentgrs_mean);
 df_owners_mean1 = engel_owners_data_percentiles(df_ASEC_hh_match_0506_final1, 10);
@@ -94,7 +94,7 @@ df_renters_mean1 = engel_renters_data_percentiles(df_ASEC_hh_match_0506_final1, 
 
 p2_mean = engel_plot_median(df_owners_mean1, df_renters_mean1, "Housing Engel Curves (ASEC ALL, 2010/2011)");
 
-# Engel curves, percentiles, median
+# Engel curves, percentiles, median of nearest neighbors
 df_ASEC_hh_match_0506_final1.valueh = df_ASEC_hh_match_0506_final1.ACS_valueh_median;
 df_ASEC_hh_match_0506_final1.rentgrs = df_ASEC_hh_match_0506_final1.ACS_rentgrs_median;
 df_owners_median1 = engel_owners_data_percentiles(df_ASEC_hh_match_0506_final1, 10);
@@ -102,26 +102,28 @@ df_renters_median1 = engel_renters_data_percentiles(df_ASEC_hh_match_0506_final1
 
 p2_median = engel_plot_median(df_owners_median1, df_renters_median1, "Housing Engel Curves (ASEC ALL, 2010/2011)");
 
-# Engel curves for property tax, median
+# Engel curves for property tax, median of nearest neigbors
 insertcols!(df_ASEC_hh_match_0506_final1, size(df_ASEC_hh_match_0506_final1, 2)+1, :proptx99_recode => df_ASEC_hh_match_0506_final.ACS_proptax_median);
+# Note: whenever the engel data preparation function has _median, it's using the median of each income bin
 df_owners_median_proptx1 = engel_proptax_data_median(df_ASEC_hh_match_0506_final1, 10);
 
 p2_proptx = proptx_plot(df_owners_median_proptx1, "Property Tax Engel Curves (ASEC ALL, 2010/2011)")
 
 
-# C. 2nd matching - no recodes
+## 2nd matching - no recodes
 matching_set = [:grossinc, :educ_recode, :unitsstr_recode];
 
 df_ASEC_hh_match_0506_final2 = ASEC_ACS_match([2010, 2011], df_ASEC_hh_match_county, df_ACS_hh_match_county, df_ASEC_hh_match_state, df_ACS_hh_match_state, matching_set);
 
+# Engel curves, percentiles, mean of nearest neighbors
 insertcols!(df_ASEC_hh_match_0506_final2, size(df_ASEC_hh_match_0506_final2, 2)+1, :valueh => df_ASEC_hh_match_0506_final2.ACS_valueh_mean);
 insertcols!(df_ASEC_hh_match_0506_final2, size(df_ASEC_hh_match_0506_final2, 2)+1, :rentgrs => df_ASEC_hh_match_0506_final2.ACS_rentgrs_mean);
 df_owners_mean2 = engel_owners_data_percentiles(df_ASEC_hh_match_0506_final2, 10);
 df_renters_mean2 = engel_renters_data_percentiles(df_ASEC_hh_match_0506_final2, 10);
-df_valueh_state_ASEC, df_proptx_state_ASEC = engel_owners_data_state(df_ASEC_hh_match_0506_final2, 10)
 
 p3_mean = engel_plot(df_owners_mean2, df_renters_mean2, "Housing Engel Curves (ASEC benchmark, 2010/2011)");
 
+# Engel curves, percentiles, median of nearest neighbors
 df_ASEC_hh_match_0506_final2.valueh = df_ASEC_hh_match_0506_final2.ACS_valueh_median;
 df_ASEC_hh_match_0506_final2.rentgrs = df_ASEC_hh_match_0506_final2.ACS_rentgrs_median;
 df_owners_median2 = engel_owners_data_percentiles_median(df_ASEC_hh_match_0506_final2, 10);
@@ -129,6 +131,7 @@ df_renters_median2 = engel_renters_data_percentiles_median(df_ASEC_hh_match_0506
 
 p3_median = engel_plot_median(df_owners_median2, df_renters_median2, "Housing Engel Curves (ASEC benchmark, 2010/2011)");
 
+# Engel curves for property tax, mean of nearest neigbors
 insertcols!(df_ASEC_hh_match_0506_final2, size(df_ASEC_hh_match_0506_final2, 2)+1, :proptx99_recode => df_ASEC_hh_match_0506_final2.ACS_proptax_mean);
 df_owners_mean_proptx2 = engel_proptax_data(df_ASEC_hh_match_0506_final2, 10);
 
@@ -166,6 +169,7 @@ matching_set = [:grossinc_potential];
 
 df_ASEC_hh_match_0506_final3 = ASEC_ACS_match([2010, 2011], df_ASEC_hh_match_county, df_ACS_hh_match_county, df_ASEC_hh_match_state, df_ACS_hh_match_state, matching_set);
 
+# Engel curves, percentiles, mean of nearest neighbors
 insertcols!(df_ASEC_hh_match_0506_final3, size(df_ASEC_hh_match_0506_final3, 2)+1, :valueh => df_ASEC_hh_match_0506_final3.ACS_valueh_mean);
 insertcols!(df_ASEC_hh_match_0506_final3, size(df_ASEC_hh_match_0506_final3, 2)+1, :rentgrs => df_ASEC_hh_match_0506_final3.ACS_rentgrs_mean);
 df_owners_mean3 = engel_owners_data_percentiles(df_ASEC_hh_match_0506_final3, 10);
@@ -173,6 +177,7 @@ df_renters_mean3 = engel_renters_data_percentiles(df_ASEC_hh_match_0506_final3, 
 
 p4_mean = engel_plot_median(df_owners_mean3, df_renters_mean3, "Housing Engel Curves (ASEC perm. INC, 2010/2011)");
 
+# Engel curves, percentiles, median of nearest neighbors
 df_ASEC_hh_match_0506_final3.valueh = df_ASEC_hh_match_0506_final3.ACS_valueh_median;
 df_ASEC_hh_match_0506_final3.rentgrs = df_ASEC_hh_match_0506_final3.ACS_rentgrs_median;
 df_owners_median3 = engel_owners_data_percentiles(df_ASEC_hh_match_0506_final3, 10);
@@ -180,6 +185,7 @@ df_renters_median3 = engel_renters_data_percentiles(df_ASEC_hh_match_0506_final3
 
 p4_median = engel_plot_median(df_owners_median3, df_renters_median3, "Housing Engel Curves (ASEC perm. INC, 2010/2011)");
 
+# Engel curves for property tax, median of nearest neigbors
 insertcols!(df_ASEC_hh_match_0506_final3, size(df_ASEC_hh_match_0506_final3, 2)+1, :proptx99_recode => df_ASEC_hh_match_0506_final3.ACS_proptax_median);
 df_owners_median_proptx3 = engel_proptax_data_median(df_ASEC_hh_match_0506_final3, 10);
 
@@ -190,14 +196,12 @@ plot(p1_mean, p2_mean, p3_mean, p4_mean, layout = (2,2), size = (1200,800))
 savefig(fig2_dir_out * "Weighted_Mean_Engel_curve_ACS_new_benchmark_median_1011.pdf");
 
 ## Compare property tax of ACS and ASEC by state
-
 df_owners_mean_ACS = engel_owners_data_state(df_ACS_hh[in([2010, 2011]).(df_ACS_hh.YEAR), :], 1);
 df_owners_mean_ASEC = engel_owners_data_state(df_ASEC_hh_match_0506_final2, 10);
 
 proptx_plot_compare!(df_owners_mean_ACS, df_owners_mean_ASEC, "/Users/jiaxitan/UMN/Fed RA/Heathcote/Property Tax Est/state engel k=9/");
 
-
-## LA vs NJ, property tax
+## LA vs NJ, property tax Engel
 s1 = "New Jersey"
 s2 = "Louisiana"
 
