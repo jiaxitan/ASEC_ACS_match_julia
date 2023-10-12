c06a5f5763d627aeb42d99997debd39a4d3f679b
jiaxitan <101900571+jiaxitan@users.noreply.github.com>
2023-10-11 20:13:39 -0500

    Use median tax rate instead of mean

--- main/code/code_functions/ACS_proptx99_topcode_imputation.jl
+++ main/code/code_functions/ACS_proptx99_topcode_imputation.jl
@@ -1,13 +1,13 @@
 
 function ACS_PROPTX99_topcode_imputation!(df)
-    df_sample = df[(df.proptx99 .== 67) .| (df.proptx99 .== 68), [:valueh, :YEAR, :proptx99, :proptx99_recode, :statefips]]
+    df_sample = df[((df.proptx99 .== 67) .| (df.proptx99 .== 68)) .& (df.ownershp .== 1), [:valueh, :YEAR, :proptx99, :proptx99_recode, :statefips]]
     df_sample.txrate = df_sample.proptx99_recode ./ df_sample.valueh
 
     df_sample.year_group .= 1
     df_sample[in([2010,2011]).(df_sample.YEAR), :year_group] .= 2
     df_sample[in([2015,2016]).(df_sample.YEAR), :year_group] .= 3
 
-    df_sample = combine(groupby(df_sample, [:year_group, :statefips]), :txrate => (x -> mean(x[(x .< 1) .& (.!ismissing.(x))])) => :txrate_topavg)
+    df_sample = combine(groupby(df_sample, [:year_group, :statefips]), :txrate => (x -> median(x[(x .< 1) .& (.!ismissing.(x))])) => :txrate_topavg)
 
     df.year_group .= 1
     df[in([2010,2011]).(df.YEAR), :year_group] .= 2
@@ -17,7 +17,7 @@ function ACS_PROPTX99_topcode_imputation!(df)
     select!(df, Not(:year_group))
     df.proptx99_imputed = copy(df.proptx99_recode)
     df.proptx99_imputed = convert(Array{Float64}, df.proptx99_imputed)
-    df[df.proptx99 .== 69, :proptx99_imputed] .= df[df.proptx99 .== 69, :valueh] .* df[df.proptx99 .== 69, :txrate_topavg]
+    df[(df.proptx99 .== 69) .& (df.ownershp .== 1), :proptx99_imputed] .= df[(df.proptx99 .== 69) .& (df.ownershp .== 1), :valueh] .* df[(df.proptx99 .== 69) .& (df.ownershp .== 1), :txrate_topavg]
 
     return df
 end
